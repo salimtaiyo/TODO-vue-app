5d6ca002c08f880f434cfce41fc53f65a284487a
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-05-05 08:05:08 -0400

    added form and submit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const app = new Vue({
     el: '#app',
     data: {
-        title : "Hello World"
+        title : "Hello World",
+        newTodo: ''
+    },
+    methods : {
+       addTodo() {
+           console.log('form submitted');
+       }
     }
 })
